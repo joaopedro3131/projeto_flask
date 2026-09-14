c87a1f8ef1f2f8db2f47f7c9a195497206a5be34
joao pedro <you@example.com>
2026-09-14 13:19:49 -0300

    .

--- app.py
+++ app.py
@@ -28,7 +28,34 @@ def lista_aluno():
 
 @app.route('/professor')
 def lista_professor():
-    return render_template('professor/lista.html')
+    # Conecta ao banco de dados
+    DB_PATH = "banco_escola.db"
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    # Executa consulta SQL
+    cursor.execute('SELECT id, nome, disciplina FROM professor')
+    # Obtém todos os registros
+    lista = cursor.fetchall()
+    # Fecha conexão
+    conn.close()
+    return render_template('professor/lista.html', lista=lista)
+
+
+
+@app.route('/turma')
+def lista_turma():
+    # Conecta ao banco de dados
+    DB_PATH = "banco_escola.db"
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    # Executa consulta SQL
+    cursor.execute('select id, semestre, curso.nome, professor.nome from turma join curso on curso.id=turma.curso_id join professor on professor.id=turma.professor_id')
+    # Obtém todos os registros
+    lista = cursor.fetchall()
+    # Fecha conexão
+    conn.close()
+    return render_template('turma/lista.html', lista=lista)
+
 
 
 @app.route('/contato')
